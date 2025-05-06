d6f9f16ea3e62c95e53a1b4818e926ddc4690c7d
ywallis <yann.wallis@gmail.com>
2025-05-06 15:41:17 +0800

    added item models

--- src/models.py
+++ src/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from sqlmodel import Field, Relationship, SQLModel
 
 
@@ -8,6 +9,7 @@ class ApartmentBase(SQLModel):
 class Apartment(ApartmentBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
     users: list["User"] = Relationship(back_populates="apartment")
+    items: list["Item"] = Relationship(back_populates="apartment")
 
 
 class ApartmentPublic(ApartmentBase):
@@ -49,3 +51,37 @@ class UserUpdate(SQLModel):
     email: str | None = None
     hashed_password: str | None = None
     apartment_id: int | None = None
+
+
+class ItemBase(SQLModel):
+    name: str
+    apartment_id: int | None = Field(default=None, foreign_key="apartment.id")
+    is_bill: bool
+    initial_value: float
+    purchase_date: datetime
+    yearly_depreciation: float
+    minimum_value: float | None
+    minimum_value_pct: float | None
+
+
+class Item(ItemBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    apartment: Apartment = Relationship(back_populates="items")
+
+
+class ItemPublic(ItemBase):
+    id: int
+
+
+class ItemCreate(ItemBase):
+    pass
+
+
+class ItemUpdate(SQLModel):
+    name: str | None = None
+    is_bill: bool | None = None
+    initial_value: float | None = None
+    purchase_date: datetime | None = None
+    yearly_depreciation: float | None = None
+    minimum_value: float | None = None
+    minimum_value_pct: float | None = None
