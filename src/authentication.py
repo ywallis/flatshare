9f70c11bbbf1f03c2ff59ad07afa7f8ad7a7dd2d
ywallis <yann.wallis@gmail.com>
2025-06-09 20:55:24 +0200

    Added linux arm to docker releases and removed hard coded auth

--- src/authentication.py
+++ src/authentication.py
@@ -7,12 +7,18 @@ from jwt.exceptions import InvalidTokenError
 from pydantic import BaseModel
 from sqlmodel import Session, select
 
+from dotenv import load_dotenv
+import os
 from src.models import User
 from src.utils import get_session
 
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+load_dotenv()
+secret_key = os.getenv("SECRET_KEY")
+if secret_key is None:
+    raise Exception("You need to set SECRET_KEY as an environment variable")
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
@@ -33,7 +39,7 @@ def create_access_token(data: dict, expires: timedelta | None = None):
     else:
         expiration = datetime.now() + timedelta(minutes=15)
     to_encode.update({"exp": expiration})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, secret_key, algorithm=ALGORITHM)
     return encoded_jwt
 
 
@@ -41,7 +47,7 @@ async def get_current_user(
     *, session: Session = Depends(get_session), token: str = Depends(oauth2_scheme)
 ):
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        payload = jwt.decode(token, secret_key, algorithms=[ALGORITHM])
         email = payload.get("sub")
         if email is None:
             raise HTTPException(
